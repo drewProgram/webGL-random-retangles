6ccf70abd6685839b139be79e2e9ab4e3089c561
Andrew Filgueiras <andrew.lascasas@gmail.com>
2020-09-25 22:55:13 -0300

    feat: rectangles generated

--- src/index.js
+++ src/index.js
@@ -7,33 +7,27 @@ in vec2 a_position;
 
 uniform vec2 u_resolution;
 
-// all shaders have a main function
 void main() {
-    // convert the position from pixels to 0.0 to 1.0
-    vec2 zeroToOne = a_position / u_resolution;
 
-    // convert from 0 -> 1 to 0 -> 2
-    vec2 zertoToTwo = zeroToOne * 2.0;
+  vec2 zeroToOne = a_position / u_resolution;
 
-    // convert from 0 -> 2 to -1 -> +1 (clip space)
-    vec2 clipSpace = zertoToTwo - 1.0;
+  vec2 zeroToTwo = zeroToOne * 2.0;
 
-    // gl_Position is a special var a vertex shader is responsible for setting
-    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+  vec2 clipSpace = zeroToTwo - 1.0;
+
+  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
 `;
 
 let fragmentShaderSource = `#version 300 es
-
 precision highp float;
 
-uniform vec4 color;
+uniform vec4 u_color;
 
-//  we need to declare an output for the fragment shader
 out vec4 outColor;
 
 void main() {
-    outColor = color;
+  outColor = u_color;
 }
 `
 
@@ -58,28 +52,18 @@ function main() {
 
     // Create a buffer
     const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // three 2d points
-    const positions = [
-        10, 20,
-        80, 20,
-        10, 30,
-        10, 30,
-        80, 20,
-        80, 30,
-    ];
-    // * Copies the positions array content into an Float array.
-    // * gl.bufferData copies that data to the positionBuffer on the GPU. It's using position buffer
-    // because we bound it to the ARRAY_BUFFER bind point above.
-    // * gl.STATIC_DRAW is a hint to webGL about how we'll the data, this tell to it that
-    // we are not likely to change this data much.
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
     // Create a vertex array object (attribute state)
     const vao = gl.createVertexArray();
+
+    // and make it the one we're currently working with
     gl.bindVertexArray(vao);
+
+    // turn on the attribute
     gl.enableVertexAttribArray(positionAttributeLocation);
+
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     // how to pull the data out of the buffer
     let size = 2;             // 2 components per iteration
@@ -93,6 +77,8 @@ function main() {
     );
 
     resizeCanvas(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     // clear canvas
@@ -106,15 +92,47 @@ function main() {
     // to clip space in the shader
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-    // bind the attribute/buffer set we want
-    gl.bindVertexArray(vao);
+    // draw 50 random rectangles in random colors
+    for (let i = 0; i < 50; i++) {
+        // Put a rectangle in the position buffer
+        setRectangle(
+            gl, randomInt(1300), randomInt(300), randomInt(300), randomInt(300));
 
-    // ASK WEBGL TO EXECUTE OUR GLSL PROGRAM
-    let primitiveType = gl.TRIANGLES;
-    offset = 0;
-    // times vertex shader need to execute (3 vertex, so 3 times)
-    let count = 6;
-    gl.drawArrays(primitiveType, offset, count);
+        // Set a random color.
+        gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
+
+        // Draw the rectangle.
+        const primitiveType = gl.TRIANGLES;
+        offset = 0;
+        const count = 6;
+        gl.drawArrays(primitiveType, offset, count);
+    }
+}
+
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
+
+// Fill the buffer with the values that define a rectangle.
+function setRectangle(gl, x, y, width, height) {
+    const x1 = x;
+    const x2 = x + width;
+    const y1 = y;
+    const y2 = y + height;
+
+    // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+    // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+    // but so far we only have one buffer. If we had more than one
+    // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]), gl.STATIC_DRAW);
 }
 
 main();
